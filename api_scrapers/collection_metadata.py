5d91ffb9e4b9fbba05a8c3c2c4fa3b582b9e1220
noahbjohnson <noah@noahbjohnson.net>
2020-07-06 13:11:05 -0500

    collection metadata typing

--- api_scrapers/collection_metadata.py
+++ api_scrapers/collection_metadata.py
@@ -1,28 +1,41 @@
 import json
+from typing import List, TypedDict
 
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
-def get_collection_metadata(driver: webdriver.Chrome, dbid: str):
+class CollectionMetadata(TypedDict, total=False):
+    dbid: str
+    database_name: str
+    levels: List[str]
+    category_name: str
+    category_id: str
+    can_save: bool
+    publication_year: int
+
+
+def get_collection_metadata(driver: webdriver.Chrome, dbid: str) -> CollectionMetadata:
     """Queries the api for the given collection id"""
-    url = f"https://www.ancestry.com/imageviewer/api/media/info-by-id?dbId={dbid}"
+    url: str = f"https://www.ancestry.com/imageviewer/api/media/info-by-id?dbId={dbid}"
     driver.get(url)
-    pre = driver.find_element_by_tag_name("pre")
+    pre: WebElement = driver.find_element_by_tag_name("pre")
     parsed = json.loads(pre.text)
     image_info = parsed['imageInfo']
-    dbid = image_info['dbId']
-    database_name = image_info["collectionInfo"]['databaseName']
-    navigation_levels = list(image_info['structureType'].values())
-    category_name = image_info["collectionInfo"]['primaryCategoryName']
-    category_id = image_info["collectionInfo"]['primaryCategoryId']
-    image_savable = image_info["collectionInfo"]['canSaveImage']
-    publish_year = image_info['collectionInfo']['publicationYear']
-    return {
-        "dbid":             dbid,
-        "database_name":    database_name,
-        "levels":           navigation_levels,
-        "category_name":    category_name,
-        "category_id":      category_id,
-        "can_save":         image_savable,
-        "publication_year": publish_year
+    dbid: str = str(image_info['dbId'])
+    database_name: str = str(image_info["collectionInfo"]['databaseName'])
+    navigation_levels: List[str] = list(image_info['structureType'].values())
+    category_name: str = str(image_info["collectionInfo"]['primaryCategoryName'])
+    category_id: str = str(image_info["collectionInfo"]['primaryCategoryId'])
+    image_savable: bool = bool(image_info["collectionInfo"]['canSaveImage'])
+    publish_year: int = int(image_info['collectionInfo']['publicationYear'])
+    metadata: CollectionMetadata = {
+        'dbid':             dbid,
+        'database_name':    database_name,
+        'levels':           navigation_levels,
+        'category_name':    category_name,
+        'category_id':      category_id,
+        'can_save':         image_savable,
+        'publication_year': publish_year
     }
+    return metadata
