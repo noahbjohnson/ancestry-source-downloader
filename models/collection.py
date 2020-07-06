8a97b5d35652643793c01766dc17e1406bcf4c14
noahbjohnson <noah@noahbjohnson.net>
2020-07-06 12:31:55 -0500

    collection database model

---
+++ models/collection.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, Integer, Sequence, String, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Collection(Base):
+    """An ancestry.com record collection"""
+    __tablename__ = 'collection'
+    id = Column(Integer, Sequence('collection_section_id_seq'), primary_key=True)
+    # Ancestry collection id
+    collection_id = Column(String(64), nullable=False)
+    # name (ie 1790 census)
+    collection_title = Column(String(256), nullable=True)
+    # If the collection is a yearbook
+    is_yearbook_collection = Column(Boolean, default=False)
+    # pipe-delimited array
+    navigation_levels = Column(String(256), nullable=True)
+    # ie cen_1790
+    category_name = Column(String(256), nullable=True)
+    # ie 170
+    category_id = Column(String(256), nullable=True)
+    # ie 1790usfedcen
+    database_name = Column(String(256), nullable=True)
+    # 1790
+    publication_year = Column(String(256), nullable=True)
+
+    time_created = Column(TIMESTAMP, default=datetime.utcnow, nullable=False)
+    time_updated = Column(TIMESTAMP, onupdate=datetime.utcnow, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Collection(id='%s')>" % self.collection_id
