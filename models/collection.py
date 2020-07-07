c6e974a2a177fe085cbce5bb931bcee9674f0be0
noahbjohnson <noah@noahbjohnson.net>
2020-07-06 20:15:45 -0500

    better collection types

--- models/collection.py
+++ models/collection.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 
 from sqlalchemy import Boolean, Column, Integer, Sequence, String, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,7 +12,7 @@ class Collection(Base):
     __tablename__ = 'collection'
     id = Column(Integer, Sequence('collection_section_id_seq'), primary_key=True)
     # Ancestry collection id
-    collection_id = Column(String(64), nullable=False)
+    collection_id = Column(String(64), nullable=False, index=True, unique=True)
     # name (ie 1790 census)
     collection_title = Column(String(256), nullable=True)
     # If the collection is a yearbook
@@ -25,10 +26,16 @@ class Collection(Base):
     # ie 1790usfedcen
     database_name = Column(String(256), nullable=True)
     # 1790
-    publication_year = Column(String(256), nullable=True)
+    publication_year = Column(Integer, nullable=True, index=True)
 
     time_created = Column(TIMESTAMP, default=datetime.utcnow, nullable=False)
     time_updated = Column(TIMESTAMP, onupdate=datetime.utcnow, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return "<Collection(id='%s')>" % self.collection_id
+
+    def get_levels(self) -> List[str]:
+        return self.navigation_levels.split("|")
+
+    def set_levels(self, levels: List[str]):
+        self.navigation_levels = "|".join(levels)
